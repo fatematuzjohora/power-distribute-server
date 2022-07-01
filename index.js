4e10317ccdf9e610dc5db5aa5942c0fc1f1c3003
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-07-01 12:31:51 +0600

    simple find api and post api done

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fytgj0e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const billingCollection = client
+      .db("PowerDistribute")
+      .collection("billingList");
+    app.get("/billing-list", async (req, res) => {
+      const query = {};
+      const cursor = billingCollection.find(query);
+      const billingList = await cursor.toArray();
+      res.send(billingList);
+    });
+
+    app.post("/billing-list", async (req, res) => {
+      const addBilling = req.body;
+      const result = await billingCollection.insertOne(addBilling);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("welcome to power distribute");
+});
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
